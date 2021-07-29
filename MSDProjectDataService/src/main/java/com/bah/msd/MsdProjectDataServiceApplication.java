56ef6ba64141764434d27dc243b07d42eab0ba6d
Emily Dougherty <dougheea@rose-hulman.edu>
2021-07-29 13:57:47 -0400

    added the spring project to the repo

---
+++ MSDProjectDataService/src/main/java/com/bah/msd/MsdProjectDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdProjectDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsdProjectDataServiceApplication.class, args);
+	}
+
+}
