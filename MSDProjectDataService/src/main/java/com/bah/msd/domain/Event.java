365c8a6a637a6d4c1e9fe33fd5315e2dc6beef61
Emily Dougherty <dougheea@rose-hulman.edu>
2021-07-29 15:33:56 -0400

    implemented the event class in the domain package

--- MSDProjectDataService/src/main/java/com/bah/msd/domain/Event.java
+++ MSDProjectDataService/src/main/java/com/bah/msd/domain/Event.java
@@ -1,5 +1,58 @@
 package com.bah.msd.domain;
 
 public class Event {
+	int id;
+	String code;
+	String title;
+	String description;
+	
+	public Event() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Event(int id, String code, String title, String description) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.title = title;
+		this.description = description;
+	}
+
+	public String toJSON(){
+		return "{\"id:\"" + id + ", \"code:\"" + code + ", \"title:\"" + title + ", \"description:\"" + description + " }";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 
 }
