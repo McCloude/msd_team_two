f0f65a02ab2c985a310f236be5943ece7a84c350
Stephen Duncan <steve@jlsc.net>
2021-08-02 14:16:36 -0400

    all template code

--- MSDProjectDataService/src/main/java/com/bah/msd/api/TestAPI.java
+++ MSDProjectDataService/src/main/java/com/bah/msd/api/TestAPI.java
@@ -1,11 +1,11 @@
-package com.bah.msd.rest;
+package com.bah.msd.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-public class TestResource {
+public class TestAPI {
 	
 	@RequestMapping
 	public String getMessage() {
--- MSDProjectDataService/src/main/java/com/bah/msd/config/SpringRepositoryConfig.java
+++ MSDProjectDataService/src/main/java/com/bah/msd/config/SpringRepositoryConfig.java
@@ -3,14 +3,14 @@ package com.bah.msd.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.bah.msd.repository.InMemoryCustomerRepository;
+import com.bah.msd.repository.CustomersRepository;
 
 @Configuration
 public class SpringRepositoryConfig {
 
-	@Bean
-	public InMemoryCustomerRepository getRepo() {
-		return new InMemoryCustomerRepository();
-	}
+//	@Bean
+//	public CustomersRepository getRepo() {
+//		return new CustomersRepository();
+//	}
 	
 }
